092153c063f4a322ae92aee36f9735aef1d5a264
Olumide Johnson <57791906+ojohnsonolanrewaju@users.noreply.github.com>
2022-10-31 19:06:46 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,60 +1,53 @@
-(function()  {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-      <style>
-      </style>
+(function () {
+  let tmpl = document.createElement("template");
+  tmpl.innerHTML = `
+      <h1>
+      Hello World
+      </h1>
     `;
+  // the tag
+  customElements.define(
+    "com-sap-sample-helloworld1",
+    class Helloworld1 extends HTMLElement {
+      //a standard web components callback and is called once, at initial widget instantiation
+      constructor() {
+        super();
+        let shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(tmpl.content.cloneNode(true));
+      }
 
-    customElements.define('com-sap-sample-template', class WidgetTemplate extends HTMLElement {
+      //Fired when the widget is added to the html DOM of the page
+      connectedCallback() {
+        this._firstConnection = true;
+        this.redraw();
+      }
 
+      //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+      disconnectedCallback() {}
 
-		constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
-		}
+      //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+      onCustomWidgetBeforeUpdate(oChangedProperties) {}
 
-
-        //Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
+      //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+      onCustomWidgetAfterUpdate(oChangedProperties) {
+        if (this._firstConnection) {
+          this.redraw();
         }
+      }
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
-        }
+      //When the custom widget is removed from the canvas or the analytic application is closed
+      onCustomWidgetDestroy() {}
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
-
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection){
-                this.redraw();
-            }
-        }
-        
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
-        }
-
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
+      //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+      // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+      //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+      /*
         onCustomWidgetResize(width, height){
         
         }
         */
 
-        redraw(){
-        }
-    
-    
-    });
-        
+      redraw() {}
+    }
+  );
 })();
